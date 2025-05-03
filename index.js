1ba3f1db2a17e29fdb9035d9ab90a24b241d2bac
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2025-05-03 17:10:07 -0300

    v1.0.7
    perdi noção da vida

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-// Importações
-const { GatewayIntentBits, Client, Collection } = require("discord.js");
-const express = require('express');
-const server = express();
-require('dotenv').config();
-
-// Inicialização do servidor Express
-server.get('/', (req, res) => {
-  return res.json({ message: 'Nossa API está online' });
-});
-
-server.listen(3000, () => {
-  console.log('Servidor Express está funcionando na porta 3000...');
-});
-
-// Criação do cliente Discord
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildVoiceStates,
-  ]
-});
-
-client.slashCommands = new Collection();
-client.prefixCommands = new Collection();
-
-// Carregar as APIs e handlers
-// require("./api.js")(client);
-require("./Handler")(client);
-
-// Processamento de erros não tratados (opcional)
-// Para capturar promessas não tratadas
-process.on('unhandledRejection', (reason, promise) => {
-  console.log(`🚫 Erro detectado na promise não tratada:`, reason, promise);
-});
-
-// Para capturar exceções não tratadas (exceções de código)
-process.on('uncaughtException', (error, origin) => {
-  console.log(`🚫 Exceção não tratada detectada:`, error, origin);
-});
-
-// Login do bot
-client.login(process.env.clientToken).then(() => {
-  console.log(`✅ Bot logado com sucesso!`);
-}).catch((err) => {
-  console.log(`❌ Erro ao logar o bot: ${err}`);
-});
+const { GatewayIntentBits, Client, Collection } = require("discord.js")
+ const express = require('express');
+ const server = express();
+ require('dotenv').config()
+ 
+ server.get('/', (req, res) => {
+   return res.json({ Retorno: 'Nossa APi está online' })
+ })
+ 
+ server.listen(3000, () => {
+   console.log('Servidor está funcionando...')
+ })
+ 
+ const client = new Client({
+   intents: [
+     GatewayIntentBits.Guilds,
+     GatewayIntentBits.GuildMessages,
+     GatewayIntentBits.MessageContent,
+     GatewayIntentBits.GuildMembers,
+     GatewayIntentBits.GuildVoiceStates,
+   ]
+ });
+ 
+ module.exports = client;
+ 
+ client.slashCommands = new Collection();
+ client.prefixCommands = new Collection();
+ 
+ //require("./api.js")(client);
+ require("./Handler")(client);
+ 
+ process.on('unhandRejection', (reason, promise) => {
+   console.log(`🚫 Erro Detectado:\n\n` + reason, promise);
+  });
+ 
+  process.on('uncaughtException', (error, origin) => {
+   console.log(`🚫 Erro Detectado:\n\n` + error, origin);
+ });
+ 
+ client.login(process.env.clientToken).then(() => {
+   console.log(`✅ Bot logado com sucesso!`);
+ }).catch((err) => {
+   console.log(`❌ Erro ao logar o bot: ${err}`);
+ });
